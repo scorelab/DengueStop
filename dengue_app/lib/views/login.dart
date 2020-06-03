268398a705caf235600efd322e961123d8fccf0a
DonOmalVindula <42619922+DonOmalVindula@users.noreply.github.com>
2020-06-04 02:04:25 +0530

    added index route with dummy login component

---
+++ dengue_app/lib/views/login.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // todo complete login screen
+    return Container();
+  }
+}
