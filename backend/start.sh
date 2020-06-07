fc9f6265e661e8d20522cef968223ff919657675
DonOmalVindula <42619922+DonOmalVindula@users.noreply.github.com>
2020-06-08 02:39:41 +0530

    #2 added http dart package, tested connection with getting response locally hosted flask code via app

--- backend/start.sh
+++ backend/start.sh
@@ -3,10 +3,12 @@
 # RUN THIS TO RUN FLASK BACKEND EVERYTIME THE PROJECT IS RUN
 
 # activating the environment
+echo 'Activating Environment...'
 . venv/Scripts/activate
 
 # selecting flask init file
 export FLASK_APP=init.py
 
 # running FLASK server
+# only used for test purposes not to be used in production!!!
 flask run
--- dengue_app/lib/views/login.dart
+++ dengue_app/lib/views/login.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class Login extends StatelessWidget {
+
+  void testFlaskConnection() async {
+    var url = 'http://0.0.0.0:5000/';
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+//      var itemCount = jsonResponse['totalItems'];
+      print('Name: ${jsonResponse['name']}');
+      print('Email: ${jsonResponse['email']}');
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // todo complete login screen
-    return Container();
+    return Container(
+      child: RaisedButton(onPressed: (){
+        testFlaskConnection();
+      },
+      child: Text("TEST FLASK"),),
+    );
   }
 }
