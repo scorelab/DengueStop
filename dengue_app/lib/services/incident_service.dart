0d2edadc649ef81aa08d3e22f7fb23a55b3a4123
DonOmalVindula <42619922+DonOmalVindula@users.noreply.github.com>
2020-06-25 01:08:21 +0530

    Update incident service with retrive incidents reported based on the user

--- dengue_app/lib/services/incident_service.dart
+++ dengue_app/lib/services/incident_service.dart
@@ -1,4 +1,5 @@
 import 'package:dengue_app/models/incident.dart';
+import 'package:dengue_app/models/user.dart';
 import 'package:dengue_app/networking/ApiProvider.dart';
 import 'package:dengue_app/services/user_service.dart';
 import 'dart:convert';
@@ -6,6 +7,27 @@ import 'dart:convert';
 class IncidentService {
   final apiProvider = ApiProvider();
   final userService = UserService();
+
+  List<Incident> incidentList = List<Incident>();
+
+  Future<List<Incident>> getIncidentsByUser() async {
+    // getting the user id
+    User currentUser = await userService.getUserData();
+    String currentUserId = currentUser.id.toString();
+    var url = 'get_incidents_by_user/' + currentUserId;
+    List<dynamic> responseData = new List<dynamic>();
+    var responseJson = await apiProvider.get(url);
+    if (responseJson['code'] == 200) {
+      responseData = responseJson['data'];
+      if (responseData.length > 0) {
+        for (int i = 0; i < responseData.length; i++) {
+          Map<String, dynamic> map = responseData[i];
+          incidentList.add(Incident.fromJson(map));
+        }
+      }
+    }
+    return incidentList;
+  }
 
   dynamic dateTimeEncode(dynamic item) {
     // this converts DateTime objects to JSON
