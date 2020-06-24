67411667e4333a6cbb880b3aecad881af8d3e790
DonOmalVindula <42619922+DonOmalVindula@users.noreply.github.com>
2020-06-25 00:56:31 +0530

    Update init.sh script to run flask-migrate on initialization

--- backend/init.sh
+++ backend/init.sh
@@ -14,5 +14,14 @@ py -3 -m venv venv
 # installing required dependencies from the requirements.txt file
 pip install -r requirements.txt
 
+# initialize flask-migrate repository
+flask db init
+
+# migrate the new changes into the flask-migrate repository/folder
+flask db migrate
+
+# confirm changes and do the initial db operations such as creating tables with relations
+flask db upgrade
+
 # exiting virtualenv
 deactivate
