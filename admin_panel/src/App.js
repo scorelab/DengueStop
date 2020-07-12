cc44ca952d4e1e33a27c552acd0807e9679353f2
Don <omalvindula@live.com>
2020-07-12 15:48:21 +0530

    Initializing react admin panel

---
+++ admin_panel/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/dashboard/index";
+
+function App() {
+    return (
+        <div>
+            <Dashboard></Dashboard>
+        </div>
+    );
+}
+
+export default App;
