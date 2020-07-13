44d0e21e40cdfaf202797656605e16a101ba9779
Don <omalvindula@live.com>
2020-07-14 04:31:00 +0530

    Add province-wise filtering to heat map

--- admin_panel/src/components/home/heatMap.js
+++ admin_panel/src/components/home/heatMap.js
@@ -2,40 +2,121 @@ import React, { useState, useEffect } from "react";
 import { Map, TileLayer, CircleMarker, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./home.css";
-import { MDBRow, MDBCol } from "mdbreact";
+import {
+    MDBRow,
+    MDBCol,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBDropdown,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+    MDBDropdownToggle,
+    MDBIcon,
+} from "mdbreact";
+import Moment from "react-moment";
 import IncidentService from "../../services/incidentService";
+import CommonService from "../../services/commonService";
 
 const HeatMap = (props) => {
     const incidentService = new IncidentService();
+    const commonService = new CommonService();
     const [markers, setMarkers] = useState([]);
+    const [provinces, setProvinces] = useState([]);
+    const [lastRefresh, setLastRefresh] = useState(Date());
+    const [selectedProvince, setSelectedProvince] = useState("all");
     // this defines the center of the map at initial load
-    const mapCenter = [7.753016, 80.747452];
-    const dummyMarkers = [
-        {
-            position: [7.753016, 80.747452],
-            content: "this is teh center",
-        },
-    ];
+    const mapCenter = [7.9, 80.747452];
     useEffect(() => {
-        incidentService.getIncidentMarkers("all").then((res) => {
-            setMarkers(res);
+        var tempArr = [];
+        commonService.getProvinceNames().then((res) => {
+            res.forEach((data) => {
+                tempArr.push(data);
+            });
+            setProvinces(tempArr);
         });
     }, []);
-    console.log(markers);
+
+    useEffect(() => {
+        incidentService.getIncidentMarkers(selectedProvince).then((res) => {
+            setMarkers(res);
+            setLastRefresh(Date());
+        });
+    }, [selectedProvince]);
+
     return (
-        <div>
+        <MDBCardBody>
             <MDBRow>
-                <MDBCol>
-                    <Map className="heat-map" center={mapCenter} zoom={8}>
-                        <TileLayer
-                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <MarkersList markers={markers} />
-                    </Map>
+                <MDBCol size="6">
+                    <h4>
+                        <b>Dengue Heat Map</b>
+                    </h4>
+                </MDBCol>
+                <MDBCol size="6" className="px-1 text-right">
+                    <ProvinceList
+                        provinces={provinces}
+                        setSelectedProvince={setSelectedProvince}
+                        selectedProvince={selectedProvince}
+                    />
                 </MDBCol>
             </MDBRow>
-        </div>
+            <div className="mt-1 heat-map-container">
+                <MDBRow>
+                    <MDBCol>
+                        <Map className="heat-map" center={mapCenter} zoom={8}>
+                            <TileLayer
+                                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <MarkersList markers={markers} />
+                        </Map>
+                        <p className="event-coordinator-text text-right">
+                            Last refresh <Moment fromNow>{lastRefresh}</Moment>
+                        </p>
+                    </MDBCol>
+                </MDBRow>
+            </div>
+        </MDBCardBody>
+    );
+};
+
+const ProvinceList = (props) => {
+    const provinces = props.provinces;
+    const setSelectedProvince = props.setSelectedProvince;
+    const selectedProvince = props.selectedProvince;
+
+    function changeProvince(data) {
+        setSelectedProvince(data);
+    }
+
+    const provinceDropdownList = provinces.map((data, key) => {
+        const province = data[0];
+        if (province) {
+            return (
+                <MDBDropdownItem
+                    key={key}
+                    onClick={() => changeProvince(province)}
+                >
+                    {province}
+                </MDBDropdownItem>
+            );
+        }
+        return null;
+    });
+
+    return (
+        <MDBDropdown className="px-1 text-right">
+            <MDBDropdownToggle className="black-text text-uppercase" nav caret>
+                {selectedProvince}
+                {"  "}
+                <MDBIcon icon="filter" className="black-text" />
+            </MDBDropdownToggle>
+            <MDBDropdownMenu className="dropdown-default ">
+                <MDBDropdownItem onClick={() => changeProvince("all")}>
+                    All
+                </MDBDropdownItem>
+                {provinceDropdownList}
+            </MDBDropdownMenu>
+        </MDBDropdown>
     );
 };
 
